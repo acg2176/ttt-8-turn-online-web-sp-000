f758fff0c831bd3a42708b489a94d99873b1a21e
Anna Guerrero <acg2176@columbia.edu>
2020-03-21 19:29:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,11 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if
+  if valid_move?(board,index)
+    move(board, index)
+  else
+    puts "invalid"
+  end
 
 
 end
